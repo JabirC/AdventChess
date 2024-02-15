22e848194fb3a61947c157efe22c26e32aa6f6b0
Jabir Chowdhury <jabirchowdhury21@gmail.com>
2024-02-15 18:53:56 -0500

    Implemented CheckMate check and tests

--- backend/src/main/java/com/example/adventchess/chessgame/logic/ChessGame.java
+++ backend/src/main/java/com/example/adventchess/chessgame/logic/ChessGame.java
@@ -35,7 +35,7 @@ public class ChessGame {
         int[] kingPosition = king.getCurrentPosition();
         String opponentColor = color.equals("White") ? "Black" : "White";
 
-        // Check if opponent pieces contain the king's position as a valid move
+        // Check if any opponent piece can attack the king's position
         for(ChessPiece opponentPiece: board.getPieces(opponentColor)){
             if(opponentPiece.isValidMove(board, kingPosition[0], kingPosition[1])){
                 return true;
@@ -45,20 +45,77 @@ public class ChessGame {
     }
 
     public boolean isCheckMated(String color){
+        // Player is in check
         if(isChecked(color)){
-            return true;
+            // King cannot make a move
+            if(!hasLegalKingMoves(color)){
+                // No piece can make a move
+                if(!hasLegalPieceMoves(color)){
+                    return true;
+                }
+            }
         }
         return false;
     }
 
-    // public boolean hasLegalKingMoves(String color) {
-    //     ChessPiece king = board.getKing(color);
-    //     int[] currentPosition = king.getCurrentPosition();
-    //     List<int[]> possibleMoves = king.possibleMoves(board);
+    public boolean isStaleMate(String color){
+        // Check if the player is in check
+        if(isChecked(color)){
+            return false;
+        }
 
-    //     for(int[] move: possibleMoves){
-    //         ChessPiece deleted = board.simulate(king, move);
-    //     }
-    //     return true;
-    // }
+        // Check if the player has no legal moves
+        if(!hasLegalPieceMoves(color)){
+            return true;
+        }
+
+        // If neither condition is met, it's not a stalemate
+        return false;
+    }
+
+    public boolean hasLegalKingMoves(String color){
+        ChessPiece king = board.getKing(color);
+        int[] currentPosition = king.getCurrentPosition();
+        List<int[]> possibleMoves = king.possibleMoves(board);
+
+        // Check if move will lead to a check
+        for(int[] move: possibleMoves){
+            // Simulate the move
+            ChessPiece deleted = board.movePiece(king, move);
+
+            // Check if the king is not in check after the move
+            if(!isChecked(color)){
+                // Undo the move 
+                board.reverseMove(deleted, move, currentPosition);
+                return true;
+            }
+
+            // Undo the move as it didn't lead to a legal move
+            board.reverseMove(deleted, move, currentPosition);
+        }
+        return false;
+    }
+
+    public boolean hasLegalPieceMoves(String color){
+        ArrayList<ChessPiece> pieces = board.getPieces(color);
+        
+        for(ChessPiece piece: pieces){
+            int[] currentPosition = piece.getCurrentPosition();
+            for(int[] move: piece.possibleMoves(board)){
+                // Simulate the move
+                ChessPiece deleted = board.movePiece(piece, move);
+    
+                // Check if the king is not in check after the move
+                if(!isChecked(color)){
+                    // Undo the move 
+                    board.reverseMove(deleted, move, currentPosition);
+                    return true;
+                }
+    
+                // Undo the move as it didn't lead to a legal move
+                board.reverseMove(deleted, move, currentPosition);
+            }
+        }
+        return false;
+    }
 }

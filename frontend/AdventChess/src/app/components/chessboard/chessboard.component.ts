9243aa738ca24e5eb7a3c71874fb9040304cbf2c
Jabir Chowdhury <jabirchowdhury21@gmail.com>
2024-02-25 19:09:27 -0500

    Created ChessBoard

--- frontend/AdventChess/src/app/components/chessboard/chessboard.component.ts
+++ frontend/AdventChess/src/app/components/chessboard/chessboard.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-chessboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chessboard.component.html',
   styleUrl: './chessboard.component.scss'
 })
 export class ChessboardComponent {
+  screenWidth: number = window.innerWidth;
+  screenHeight: number = window.innerHeight;
 
+  constructor() {
+    this.getScreenWidth();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  getScreenWidth(event?: Event): void {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+  }
+
+  isDarkSquare(row: number, col: number): boolean {
+    return (row + col) % 2 !== 0;
+  }
 }

5292478c24b3123160812ed29b87039b4ccd44c8
Jabir Chowdhury <jabirchowdhury21@gmail.com>
2024-03-01 17:47:19 -0500

    Board is initialized with pieces

--- frontend/AdventChess/src/app/components/chessboard/chessboard.component.ts
+++ frontend/AdventChess/src/app/components/chessboard/chessboard.component.ts
@@ -9,4 +9,44 @@ import { CommonModule } from '@angular/common';
   styleUrl: './chessboard.component.scss'
 })
 export class ChessboardComponent {
+  boardState: string[][] = [];
+
+  constructor() {
+    // Initialize the boardState with a default chessboard configuration
+    this.initializeBoard();
+  }
+
+  private initializeBoard(): void {
+    // Array representing the default starting position of pieces on a chessboard
+    const defaultBoard: string[][] = [
+      ['BR', 'BN', 'BB', 'BQ', 'BK', 'BB', 'BN', 'BR'],
+      ['BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP'],
+      ['--', '--', '--', '--', '--', '--', '--', '--'],
+      ['--', '--', '--', '--', '--', '--', '--', '--'],
+      ['--', '--', '--', '--', '--', '--', '--', '--'],
+      ['--', '--', '--', '--', '--', '--', '--', '--'],
+      ['WP', 'WP', 'WP', 'WP', 'WP', 'WP', 'WP', 'WP'],
+      ['WR', 'WN', 'WB', 'WQ', 'WK', 'WB', 'WN', 'WR'],
+    ];
+
+    // Copy the default board configuration to the boardState
+    this.boardState = defaultBoard.map(row => [...row]);
+  }
+
+  keyValuePairs = new Map([
+    ['BR', '../../../assets/pieces/rook-b.svg'],
+    ['BN', '../../../assets/pieces/knight-b.svg'],
+    ['BB', '../../../assets/pieces/bishop-b.svg'],
+    ['BQ', '../../../assets/pieces/queen-b.svg'],
+    ['BK', '../../../assets/pieces/king-b.svg'],
+    ['BP', '../../../assets/pieces/pawn-b.svg'],
+
+    ['WR', '../../../assets/pieces/rook-w.svg'],
+    ['WN', '../../../assets/pieces/knight-w.svg'],
+    ['WB', '../../../assets/pieces/bishop-w.svg'],
+    ['WQ', '../../../assets/pieces/queen-w.svg'],
+    ['WK', '../../../assets/pieces/king-w.svg'],
+    ['WP', '../../../assets/pieces/pawn-w.svg'],
+  ]);
+
 }

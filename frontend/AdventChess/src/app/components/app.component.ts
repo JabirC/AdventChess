a5516df8f011c16b002ecc1a687bcfa78f8b615b
Jabir Chowdhury <jabirchowdhury21@gmail.com>
2024-02-29 22:02:57 -0500

    Created AdventChess Logo

--- frontend/AdventChess/src/app/components/app.component.ts
+++ frontend/AdventChess/src/app/components/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChessboardComponent } from './chessboard/chessboard.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChessboardComponent],
+  imports: [RouterOutlet, ChessboardComponent, TopBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

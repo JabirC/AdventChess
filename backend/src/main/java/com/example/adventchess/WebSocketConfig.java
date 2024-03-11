50effc6ce2e3430d34faf8bd503d20c0cf360ca3
Jabir Chowdhury <jabirchowdhury21@gmail.com>
2024-03-11 18:08:39 -0400

    Implement player queue for matchmaking and private message channels for each game

--- backend/src/main/java/com/example/adventchess/WebSocketConfig.java
+++ backend/src/main/java/com/example/adventchess/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.example.adventchess;
 
+import com.example.adventchess.handler.CustomHandshakeHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -13,13 +14,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry config) {
-    config.enableSimpleBroker("/topic");
+    config.enableSimpleBroker("/topic", "/user");
     config.setApplicationDestinationPrefixes("/app");
   }
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins(url).withSockJS();
+    registry
+        .addEndpoint("/ws")
+        .setAllowedOrigins(url)
+        .setHandshakeHandler(new CustomHandshakeHandler())
+        .withSockJS();
   }
 
 }

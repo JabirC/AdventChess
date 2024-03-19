03f88dd420d4a9bd3970f6bed66f83a25e3d7c43
Jabir Chowdhury <jabirchowdhury21@gmail.com>
2024-03-19 17:41:37 -0400

    Implement top bar navigation

--- frontend/AdventChess/src/app/components/login/login.component.spec.ts
+++ frontend/AdventChess/src/app/components/login/login.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { GameComponent } from './login.component';
+import {LoginComponent } from './login.component';
 
 describe('GameComponent', () => {
-  let component: GameComponent;
-  let fixture: ComponentFixture<GameComponent>;
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GameComponent]
+      imports: [LoginComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(GameComponent);
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- frontend/AdventChess/src/app/components/top-bar/top-bar.component.ts
+++ frontend/AdventChess/src/app/components/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-bar',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './top-bar.component.scss'
 })
 export class TopBarComponent {
-
+  constructor(private router: Router){
+  }
+  gameMenu(){
+    this.router.navigate(['/mode']);
+  }
+  home(){
+    this.router.navigate(['/home']);
+  }
 }

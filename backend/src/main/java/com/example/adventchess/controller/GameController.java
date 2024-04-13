0f7cce83e98799ceb2c0d43307799c89b1d1bec5
Jabir Chowdhury <jabirchowdhury21@gmail.com>
2024-04-13 03:43:13 -0400

    Write comments

--- backend/src/main/java/com/example/adventchess/controller/GameController.java
+++ backend/src/main/java/com/example/adventchess/controller/GameController.java
@@ -1,17 +1,7 @@
 package com.example.adventchess;
 
-import com.example.adventchess.service.ChessGameService;
-import com.example.adventchess.model.MoveMessage;
-// import java.util.Map;
-import org.springframework.web.socket.WebSocketSession;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.annotation.SendToUser;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.beans.factory.annotation.Autowired;
 import java.util.UUID;
 import java.security.Principal;
-
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ExecutorService;
@@ -23,14 +13,19 @@ import java.util.Set;
 import java.util.HashMap;
 import java.util.Map;
 
-
-// import java.lang.InterruptedException;
-
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
+import com.example.adventchess.service.ChessGameService;
+import com.example.adventchess.model.MoveMessage;
 
 
 @Controller
@@ -49,10 +44,14 @@ public class GameController {
     this.simpMessagingTemplate = simpMessagingTemplate;
     this.chessGameService = chessGameService;
 
+    // Start asynchronous background thread to handle matchmaking
     startQueueProcessor(sessionQueueClassic, "classic");
     startQueueProcessor(sessionQueueAdventure, "adventure");
   }
 
+ /**
+  *  Place players in queue depending on game mode
+  */
   @MessageMapping("/connect/game")
   public void handleConnect(Principal principal, SimpMessageHeaderAccessor headerAccessor, String mode) {
         String session = principal.getName();
@@ -71,36 +70,50 @@ public class GameController {
   }
 
 
-
+ /**
+  *  Handle multiple types of player disconnections such as page reload, component destruction, and resignation
+  */
   @MessageMapping("/disconnect")
   public void disconnect(Principal principal, String reason) {
       String session = principal.getName();
       chessGameService.handleDisconnect(session, reason);
   }
 
+ /**
+  *  Handles rematch functionality
+  */
   @MessageMapping("/game/{gameId}/rematch")
   public void handleRematch(@DestinationVariable String gameId, Principal principal, String mode) {
       String session = principal.getName();
       chessGameService.handleRematch(gameId, session, mode);
   }
 
+ /**
+  *  Verification of player moves within a game
+  */
   @MessageMapping("/game/{gameId}/move")
   public void handleMove(@DestinationVariable String gameId, Principal principal, MoveMessage moveMessage) {
         String session = principal.getName();
         chessGameService.verifyMove(session, gameId, moveMessage);
   }
   
-
+ /**
+  *  Used to check for user availabity when matching user for a new game
+  */
   @MessageMapping("/pong")
   public void handlePing(Principal principal, String message) {
         String session = principal.getName();
         pings.add(session);
   }
 
+ /**
+  *  Background asynchronous thread that handles matchmaking for a given game queue
+  */
   private void startQueueProcessor(BlockingQueue<String> sessionQueue, String mode) {
     executorService.execute(() -> {
         while (true) {
             try {
+                // Take two players from the queue and check their connection
                 String session1 = sessionQueue.take();
                 String session2 = sessionQueue.take();
                 CompletableFuture<Boolean> heartbeatCheck1 = checkHeartbeatAsync(session1);
@@ -109,8 +122,10 @@ public class GameController {
                 // Perform actions asynchronously when heartbeat checks complete
                 heartbeatCheck1.thenAccept(result1 -> {
                     heartbeatCheck2.thenAccept(result2 -> {
+                        // both players are connected
                         if (result1 && result2) {
                             chessGameService.createGameSession(session1, session2, mode);
+                        // If one player is not connected, put connected player back into queue
                         } else if (!result1 && result2) {
                             sessionQueue.add(session2);
                         } else if (result1 && !result2) {
@@ -118,8 +133,6 @@ public class GameController {
                         }
                     });
                 });
-                
-                // chessGameService.createGameSession(session1, session2, mode);
             } catch (InterruptedException e) {
                 e.printStackTrace();
                 Thread.currentThread().interrupt();
@@ -128,6 +141,9 @@ public class GameController {
     });
 }
 
+/**
+ *  Ping-pong feature to check for user connectivity
+ */
 private CompletableFuture<Boolean> checkHeartbeatAsync(String sessionId) {
   return CompletableFuture.supplyAsync(() -> {
       try {
